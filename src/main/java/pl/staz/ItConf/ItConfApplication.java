f69c3da053f4d7dc5acd67e89428f44a8addd582
MrCosik <56444584+MrCosik@users.noreply.github.com>
2021-05-28 09:49:01 +0200

    Initial commit

---
+++ src/main/java/pl/staz/ItConf/ItConfApplication.java
@@ -0,0 +1,13 @@
+package pl.staz.ItConf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItConfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/staz/ItConf/ItConfApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.staz.ItConf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
